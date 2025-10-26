bc583f435c702e4209018fa5a59b27c294814736
sortofel <sortofel@gmail.com>
2025-10-26 22:24:04 +0900

    feat: S3 설정 및 서비스에 presigned url 추가

--- src/main/java/com/seoulotakus/takumapbe/global/config/S3Config.java
+++ src/main/java/com/seoulotakus/takumapbe/global/config/S3Config.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.net.URI;
 
@@ -23,12 +24,34 @@ public class S3Config {
     @Value("${cloud.aws.credentials.secret-key:}")
     private String secretKey;
 
+    private final Region region = Region.AP_NORTHEAST_2;
+
     @Bean
     public S3Client s3Client() {
         S3ClientBuilder builder = S3Client.builder()
                 .region(Region.AP_NORTHEAST_2);
 
         // local 환경 (MinIO)
+        if (!endpoint.isEmpty()) {
+            builder = builder
+                    .endpointOverride(URI.create(endpoint))
+                    .credentialsProvider(
+                            StaticCredentialsProvider.create(
+                                    AwsBasicCredentials.create(accessKey, secretKey)
+                            )
+                    );
+        }
+
+        // prod 환경 (IAM Role)
+        return builder.build();
+    }
+
+    // == S3 Presign URL Bean
+    @Bean
+    public S3Presigner s3Presigner() {
+        S3Presigner.Builder builder = S3Presigner.builder()
+                .region(region);
+
         if (!endpoint.isEmpty()) {
             builder = builder
                     .endpointOverride(URI.create(endpoint))
