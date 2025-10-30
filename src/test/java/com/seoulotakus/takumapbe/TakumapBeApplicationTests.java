1a163c47fb115debb8d070628789606bbdfc3624
ddzeun <ddzeun@gmail.com>
2025-10-30 23:28:51 +0900

    fix: 테스트 환경 S3 Bean 추가

--- src/test/java/com/seoulotakus/takumapbe/TakumapBeApplicationTests.java
+++ src/test/java/com/seoulotakus/takumapbe/TakumapBeApplicationTests.java
@@ -1,12 +1,26 @@
 package com.seoulotakus.takumapbe;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TakumapBeApplicationTests {
+
+    @TestConfiguration
+    static class TestS3Config {
+
+        @Bean
+        public S3Client s3Client() {
+
+            return Mockito.mock(S3Client.class);
+        }
+    }
 
     @Test
     void contextLoads() {
