de4a4babbc21cceb8f63aa2c6d2d7de61d3266ec
ddzeun <ddzeun@gmail.com>
2025-10-31 02:42:15 +0900

    chore: test환경 Mock Bean 수정

--- src/test/java/com/seoulotakus/takumapbe/TakumapBeApplicationTests.java
+++ src/test/java/com/seoulotakus/takumapbe/TakumapBeApplicationTests.java
@@ -5,20 +5,28 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.test.context.ActiveProfiles;
 import software.amazon.awssdk.services.s3.S3Client;
+
+import java.util.Optional;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TakumapBeApplicationTests {
 
     @TestConfiguration
-    static class TestS3Config {
+    static class TestConfig {
 
         @Bean
         public S3Client s3Client() {
 
             return Mockito.mock(S3Client.class);
+        }
+
+        @Bean
+        public AuditorAware<Long> auditorAware() {
+            return () -> Optional.empty();
         }
     }
 
