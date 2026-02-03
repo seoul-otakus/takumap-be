9eda08ac8ccf7edf2da4cd88bc13c2a51a0c1c1f
sortofel <sortofel@gmail.com>
2026-02-03 23:29:05 +0900

    fix: category entity 중복 해결 - /shop, /category 하위의 중복된 category entity 합산

--- src/main/java/com/seoulotakus/takumapbe/domain/category/entity/Category.java
+++ src/main/java/com/seoulotakus/takumapbe/domain/category/entity/Category.java
@@ -1,23 +1,42 @@
 package com.seoulotakus.takumapbe.domain.category.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "tbl_category")
+import java.time.Instant;
+
 @Getter
+@Entity
+@Table(name = "tbl_category", schema = "takumap_db")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true, length = 50)
-    private String name;  // "가챠샵", "굿즈샵" 등
+    @Size(max = 20)
+    @Column(name = "name", length = 20)
+    private String name;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @NotNull
+    @Column(name = "created_by", nullable = false)
+    private Long createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
 
     @Builder
     public Category(String name) {
--- src/main/java/com/seoulotakus/takumapbe/domain/shop/repository/CategoryRepository.java
+++ src/main/java/com/seoulotakus/takumapbe/domain/shop/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 package com.seoulotakus.takumapbe.domain.shop.repository;
 
-import com.seoulotakus.takumapbe.domain.shop.entity.Category;
+import com.seoulotakus.takumapbe.domain.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
